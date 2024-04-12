7ed27dc313dca4b213ddf83ca2a3ab6c95459b34
Marcos Gronda <mgronda@itba.edu.ar>
2024-04-12 11:37:04 -0300

    feat: particle size in animation

--- python/src/animation.py
+++ python/src/animation.py
@@ -5,6 +5,8 @@ import numpy as np
 
 
 def generate_intermediate_positions(prev: [], post: [], steps: int):        # TODO: ver como hacerlo accurate
+    return [prev, post]
+
     resp = [[] for _ in range(steps)]
     for past, present in zip(prev, post):
         dx = (present[X] - past[X]) / steps
@@ -16,13 +18,13 @@ def generate_intermediate_positions(prev: [], post: [], steps: int):        # TO
     return resp
 
 
-def draw_new_frame(particle_data: [], obstacle_data: [], height: int, width: int, size_multiplier: int):
+def draw_new_frame(particle_data: [], obstacle_data: [], height: int, width: int, particle_radius:int, obstacle_radius: int, size_multiplier: int):
     frame = np.ones((height, width, 3), dtype=np.uint8) * 255
     for particle in particle_data:
-        cv2.circle(frame, (int(particle[X] * size_multiplier), int(particle[Y] * size_multiplier)), radius=4,  # TODO: CHANGE
+        cv2.circle(frame, (int(particle[X] * size_multiplier), int(particle[Y] * size_multiplier)), radius= particle_radius,  # TODO: CHANGE
                    color=(0, 0, 255), thickness=-1)
     for obstacle in obstacle_data:
-        cv2.circle(frame, (int(obstacle[X] * size_multiplier), int(obstacle[Y] * size_multiplier)), radius=20,  # TODO: CHANGE
+        cv2.circle(frame, (int(obstacle[X] * size_multiplier), int(obstacle[Y] * size_multiplier)), radius=obstacle_radius,  # TODO: CHANGE
                    color=(0, 255, 0), thickness=-1)
     return frame
 
@@ -32,8 +34,8 @@ def animate(particle_filename: str, static_data_filename: str, size_multiplier: 
 
     fps = 30
     steps = 4
-    screen_size = int(static_data['l'])
-    width, height = screen_size * size_multiplier, screen_size * size_multiplier
+    particle_radius, obstacle_radius = int(static_data['pr'] * size_multiplier), int(static_data['or'] * size_multiplier)
+    width, height = int(static_data['l'] * size_multiplier), int(static_data['l'] * size_multiplier)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter('../animations/simulation_video.mp4', fourcc, fps, (width, height))
 
@@ -44,7 +46,7 @@ def animate(particle_filename: str, static_data_filename: str, size_multiplier: 
         obstacle_intermediate_range = generate_intermediate_positions(particle_data[i][OBSTACLES], particle_data[i+1][OBSTACLES], steps)
 
         for p_i, o_i in zip(particle_intermediate_range, obstacle_intermediate_range):
-            frame = draw_new_frame(p_i, o_i, height, width, size_multiplier)
+            frame = draw_new_frame(p_i, o_i, height, width, particle_radius, obstacle_radius, size_multiplier)
             out.write(frame)
 
         i += 1
